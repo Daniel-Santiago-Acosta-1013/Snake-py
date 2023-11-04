9bf31392d3a0bab658bea98191423261950874f7
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-11-04 12:59:49 -0500

    [fix] mejorando paredes de opstaculo

--- entities/walls.py
+++ entities/walls.py
@@ -8,13 +8,26 @@ class Walls:
     def __init__(self, snake, food):
         self.positions = self.generate_walls(snake, food)
 
+    def generate_wall_segment(self, start, direction, length):
+        return [(start[0] + i * direction[0], start[1] + i * direction[1]) for i in range(length)]
+
     def generate_walls(self, snake, food):
         walls = set()
-        while len(walls) < 10:  # Generamos 10 paredes para empezar
-            wall = (random.randint(0, (SCREEN_WIDTH // CELL_SIZE) - 1),
-                    random.randint(0, (SCREEN_HEIGHT // CELL_SIZE) - 1))
-            if wall not in snake.body and wall != food:
-                walls.add(wall)
+        while len(walls) < 50:  # Intentaremos generar un total de 50 segmentos de pared
+            start = (random.randint(0, (SCREEN_WIDTH // CELL_SIZE) - 1),
+                     random.randint(0, (SCREEN_HEIGHT // CELL_SIZE) - 1))
+            length = random.randint(2, 5)  # Longitud aleatoria para el segmento de pared
+            if random.choice([True, False]):
+                direction = (1, 0)  # Horizontal
+            else:
+                direction = (0, 1)  # Vertical
+            
+            segment = self.generate_wall_segment(start, direction, length)
+            
+            # Comprobamos que el segmento no colisione con la comida ni con la serpiente
+            if all(pos not in snake.body and pos != food for pos in segment):
+                walls.update(segment)
+        
         return walls
 
     def draw(self, screen):
